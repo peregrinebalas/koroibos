c4919abb6d017250e5df095c219db66f6255eb2c
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-06-03 17:18:32 -0600

    Creates optional query param for age to olympian index

--- routes/olympian.js
+++ routes/olympian.js
@@ -7,15 +7,34 @@ const sequelize = require('sequelize');
 
 router.get('/olympians', async function(req, res, next) {
   try {
-    const allOlymps = await Olympian.findAll({
-      attributes: ['sport', 'team', 'age', 'name', [sequelize.fn('COUNT', sequelize.col('medal')), 'total_medals_won']],
-      group: ['Olympian.sport', 'Olympian.team', 'Olympian.age', 'Olympian.name']
-    });
+    let olymps = null;
+    if (req.query.age) {
+      if (req.query.age === 'youngest') {
+        let age = await Olympian.min('age')
+        olymps = await Olympian.findAll({
+          where: { age: age },
+          attributes: ['name', 'age', 'team', 'sport', [sequelize.fn('COUNT', sequelize.col('medal')), 'total_medals_won']],
+          group: ['Olympian.sport', 'Olympian.team', 'Olympian.age', 'Olympian.name']
+        });
+      } else if (req.query.age === 'oldest') {
+        let age = await Olympian.max('age')
+        olymps = await Olympian.findAll({
+          where: { age: age },
+          attributes: ['name', 'age', 'team', 'sport', [sequelize.fn('COUNT', sequelize.col('medal')), 'total_medals_won']],
+          group: ['Olympian.sport', 'Olympian.team', 'Olympian.age', 'Olympian.name']
+        });
+      }
+    } else {
+      olymps = await Olympian.findAll({
+        attributes: ['name', 'age', 'team', 'sport', [sequelize.fn('COUNT', sequelize.col('medal')), 'total_medals_won']],
+        group: ['Olympian.sport', 'Olympian.team', 'Olympian.age', 'Olympian.name']
+      });
+    }
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).send(JSON.stringify(allOlymps))
+    res.status(200).send(JSON.stringify({olympians: olymps}));
   } catch (error) {
     res.setHeader('Content-Type', 'application/json');
-    res.status(404).send(JSON.stringify(error))
+    res.status(404).send(JSON.stringify(error));
   }
 });
 
